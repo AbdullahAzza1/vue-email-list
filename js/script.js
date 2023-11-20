00fe253061773eaea09633850fbfa4b5755e7b3a
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-20 17:44:59 +0100

    step7

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ createApp({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
                 this.userMail = response.data.response;
             });
+            for (let i = 0; i < userMail.length; i++) {
+                userMail += mail[i]  ;
+              }
         }
     },
 
